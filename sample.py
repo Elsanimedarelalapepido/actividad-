23c157656d882218d141f47e7ada67c692859188
Elsanimedarelalapepido <an.maquera@duocuc.cl>
2025-04-30 17:15:17 -0400

    Probando commit

---
+++ sample.py
@@ -0,0 +1,12 @@
+cat > sample-app/sample_app.py <<'EOF'
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
+EOF
